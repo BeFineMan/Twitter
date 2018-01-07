b2d85469ab4ffe782fae1f91a1ff231d1977b120
handsome-111 <34496168+handsome-111@users.noreply.github.com>
2018-01-07 14:55:09 +0800

    11

--- WebRoot/js/userhome.js
+++ WebRoot/js/userhome.js
@@ -1,14 +1,5 @@
-function publish(){
-	var publish_dynamic_form = document.getElementById("publish_dynamic_form");
-	if(publish_dynamic_form != null){
-		for(var i = 0; i < publish_dynamic_form.length; i++){
-			console.log("name:"+publish_dynamic_form.elements[i].name+",value:"+publish_dynamic_form.elements[i].value
-				+"type:"+publish_dynamic_form.elements[i].type
-			);
-		}
-	}
-};
 var xmlHttp;
+
 function createXHR(){
 	if (typeof XMLHttpRequest != "undefined"){
 		return new XMLHttpRequest();
@@ -30,18 +21,17 @@ function createXHR(){
 		throw new Error("NO XHR object available.");
 	}
 }
-
-function test(){
-	var testform = document.getElementById("testform");
-	var file = document.getElementById("file");
-	var input = document.getElementById("text");
-	console.log(text);
-	console.log(file);
-	console.log(file.files(0).fileSize);
-	for(var i = 0; i < testform.length; i++){
-		console.log("name:"+testform.elements[i].name);
+function publish(){
+	createXHR();
+	xmlHttp.open("post","/Twitter/user/publishDynamic.log");
+	var publish_dynamic_form = document.getElementById("publish_dynamic_form");
+	if(publish_dynamic_form != null){
+		for(var i = 0; i < publish_dynamic_form.length; i++){
+			console.log("name:"+publish_dynamic_form.elements[i].name+",value:"+publish_dynamic_form.elements[i].value
+				+"type:"+publish_dynamic_form.elements[i].type);
+		}
 	}
-}
+};
 
 
 
